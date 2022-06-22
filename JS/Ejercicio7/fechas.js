43ad868a3f9d6b670b7df31e6c09adadcd53f1a3
Lummy <67608060+Lummy-rs@users.noreply.github.com>
2022-06-22 19:59:23 +0000

    fechas

---
+++ JS/Ejercicio7/fechas.js
@@ -0,0 +1,9 @@
+let date = new Date();
+let birthday = new Date('2001-11-19');
+let fechaAct = date;
+
+let masTarde = fechaAct > birthday;
+
+let diaB = birthday.getDate();
+let mesB = birthday.getMonth();
+let anioB = birthday.getFullYear();
